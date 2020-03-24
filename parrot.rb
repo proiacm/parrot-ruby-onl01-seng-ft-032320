3b166d0bbd9a7c3382d22fed785f8caabc0ad5b6
CiaraMaria Proia <proiacm@gmail.com>
2020-03-24 00:55:15 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot (says = "Squawk!", respond = "Pretty bird!")
+def parrot (says = "Squawk!")
   puts "#{says}"
-  puts "#{respond}"
   "Squawk!"
 end
